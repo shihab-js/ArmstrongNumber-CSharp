ed0e5037b69f287b388b33895900782c9d1bc89d
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 11:54:41 +0600

    User input completed!

--- ArmstrongNumber/Program.cs
+++ ArmstrongNumber/Program.cs
@@ -6,7 +6,15 @@ namespace ArmstrongNumber
     {
         static void Main(string[] args)
         {
-           // 371 = (3*3*3)+(7*7*7)+(1*1*1)
+            // 371 = (3*3*3)+(7*7*7)+(1*1*1)
+            //declare the necessary variables
+            int number, remainder, sum = 0, temp;
+            //print message to show the user
+            Console.Write("Please Enter a Number: ");
+            //get input from user
+            number = int.Parse(Console.ReadLine());
+            //store the number in a temporary variable
+
         }
     }
 }
